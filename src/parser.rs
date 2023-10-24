9ff6916f9dd1e3a5227f63417f0418ec96ce1b95
Rodrigo Baraglia <rodrigobaraglia@gmail.com>
2023-10-24 17:35:51 -0300

    removed redundant test

--- src/parser.rs
+++ src/parser.rs
@@ -29,22 +29,15 @@ mod test{
     fn test_from_token_vector() {
         let tokens = vec![Token::LET, Token::IDENT{begin:0,end:6}, Token::INTEGER(8)];
         let mut parser = Parser::from_tokens(tokens.into_iter());
-        println!("{:?}", parser);
-        let t1 = parser.tokens.next().unwrap_or(Token::EOF);
-        println!("{:?}", t1);
-    }
-
-    #[test]
-    fn test_next_token() {
-        let tokens = vec![Token::LET, Token::IDENT{begin:0,end:6}, Token::INTEGER(8)];
-        let mut parser = Parser::from_tokens(tokens.into_iter());
         let t1 = parser.next_token();
         println!("{:?}", t1);
+        assert_eq!(t1, Token::LET);
         let t2 = parser.next_token();
         println!("{:?}", t2);
+        assert_eq!(t2, Token::IDENT{begin:0,end:6});
         let t3 = parser.next_token();
         println!("{:?}", t3);
+        assert_eq!(t3, Token::INTEGER(8))
     }
-
 
 }
